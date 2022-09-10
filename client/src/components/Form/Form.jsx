9d1209ad25cfc7fcd157702c87f5d09993d6f7e4
Jonatan Piedra <jonatan2502@gmail.com>
2022-09-10 19:58:03 -0400

    form done

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,167 +1,96 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 
 export default function Form() {
 
-    const isDisabled = [true, true, true, true]
-
+    const minWeightRef = useRef()
+    const maxWeightRef = useRef()
+    const minHeightRef = useRef()
+    const maxHeightRef = useRef()
+    const minLifespanRef = useRef()
+    const maxLifespanRef = useRef()
+    const imageRef = useRef()
+    
     const [breed, setBreed] = useState({
         name: '',
-        min_height: '',
-        max_height: '',
-        min_weight: '',
-        max_weight: '',
-        min_life_span: '',
-        max_life_span: '',
+        minHeight: '',
+        maxHeight: '',
+        minWeight: '',
+        maxWeight: '',
+        minLifespan: '',
+        maxLifespan: '',
+        image: '',
     })
-
-    // const [errorMessage, setErrorMessage] = useState({
-    //     name: ' ',
-    //     height: ' ',
-    //     weight: ' ',
-    //     life_span: ' ',
-    // })
-
+    
     const [errorMessage, setErrorMessage] = useState({
         name: ' ',
         height: ' ',
         weight: ' ',
-        life_span: ' ',
+        lifespan: ' ',
+        image: ' ',
     })
-
-    // const [errorMessage, setErrorMessage] = useState('Empty form')
-
+    
+    // On envery change in input fields performs state updates and validates input data from user
     const handleChange = (e) => {
-        const name = e.target.name
-        //validate(e.target)
         setBreed({
             ...breed,
-            [e.target.name]: e.target.value.slice(0, 50)
-            // [e.target.name]: e.target.value.match( name == 'name' ? /^[a-zA-Z\s]*$/g : /\D/g, '')[0]
+            [e.target.name]: e.target.name === 'name' ? e.target.value.slice(0, 50) : e.target.value.replace('-', '') // limit name to 50 chars and replaces negative sign in inputs
         })
+        validate(e)
     }
-
-    // useEffect(() => {
-    //     validateName(breed)
-    //     // validateMaxsAndMins('height')
-    //     // validateMaxsAndMins('weight')
-    //     // validateMaxsAndMins('life_span')
-    // }, [breed])
-
-    useEffect(() => {
-        validateMaxsAndMins('height')
-    }, [breed])
-
-    // useEffect(() => {
-    //     validateMaxsAndMins('weight')
-    // }, [breed])
-
-    // useEffect(() => {
-    //     validateMaxsAndMins('life_span')
-    // }, [breed])
-
-    // const validateName = (breed) => {
-
-    //     errorMessage.name = !/^[a-zA-Z\s]*$/.test(breed.name) ? 'Only letters and spaces are allowed' : ''
-    //     errorMessage.name ? isDisabled[0] = true : isDisabled[0] = false
-    // }
-    const validateMaxsAndMins = (attribute) => {
-        if (Number(breed[`min_${attribute}`]) > Number(breed[`max_${attribute}`]) && Number(breed[`max_${attribute}`]) != '') {
+    
+    // Validates name and image URL input fields and calls aux function to validades numeric fields depending on the input name.
+    // Uses useRef hook in order to get actual values from input fields
+    const validate = (event) => {
+        const name = event.target.name
+        if (event.target.name === 'name' ) {
             setErrorMessage({
                 ...errorMessage,
-                [attribute]: `Min ${attribute} cannot be greater than Max ${attribute}`
+                name: !/^[a-zA-Z\s]*$/.test(event.target.value) ? 'Only letters and spaces are allowed' : ''
             })
-        } else {
+        } else if (event.target.name === 'image') {
+            const regEx = /^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/
             setErrorMessage({
                 ...errorMessage,
-                [attribute]: ''
+                image: !regEx.test(event.target.value) ? 'Enter a valid URL' : ''
             })
+        } else if (name === 'minHeight' || name === 'maxHeight') {
+            validateMaxAndMin(minHeightRef, maxHeightRef, 'height')
+        } else if (name === 'minWeight' || name === 'maxWeight') {
+            validateMaxAndMin(minWeightRef, maxWeightRef, 'weight')
+        } else if (name === 'minLifespan' || name === 'maxLifespan') {
+            validateMaxAndMin(minLifespanRef, maxLifespanRef, 'lifespan')
         }
     }
-    const validateName = (breed) => {
 
-        setErrorMessage({
-            ...errorMessage,
-            name: !/^[a-zA-Z\s]*$/.test(breed.name) ? 'Only letters and spaces are allowed' : ''
-        })
-        errorMessage.name ? isDisabled[0] = true : isDisabled[0] = false
+    // This function validates min and max input fields. Min can't be greater than max and empty fields are not allowed.
+    // Also, at first it only runs if there are values in both input fields. Finally, sets error message to an empty string meaning everything is ok.
+    const validateMaxAndMin = (minRef, maxRef, input) => {
+            if (minRef.current.value === '' && maxRef.current.value === '') {
+                setErrorMessage({
+                    ...errorMessage,
+                    [input]: 'This field is required'
+                })
+            } else if (Number(minRef.current.value) > Number(maxRef.current.value) && maxRef.current.value !== '') {   
+                setErrorMessage({
+                    ...errorMessage,
+                    [input]: `Min ${input} cannot be greater than Max ${input}`
+                })
+            } else if (minRef.current.value !== '' && maxRef.current.value !== '') {
+                setErrorMessage({
+                    ...errorMessage,
+                    [input]: ''
+                })
+            }
     }
-
-
-
-        // if (Number(breed.max_height) < Number(breed.min_height) && Number(breed.max_height) != 0) {
-        //     setErrorMessage({
-        //         ...errorMessage,
-        //         height: 'Min Height cannot be greater than Max Height'
-        //     })
-        // } else if (Number(breed.max_weight) < Number(breed.min_weight) && Number(breed.max_weight) != 0) {
-        //     setErrorMessage({
-        //         ...errorMessage,
-        //         weight: 'Min weight cannot be greater than Max weight'
-        //     })
-        // } else if (Number(breed.max_life_span) < Number(breed.min_life_span) && Number(breed.max_life_span) != 0) {
-        //     setErrorMessage({
-        //         ...errorMessage,
-        //         life_span: 'Min life span cannot be greater than Max life span'
-        //     })
-        // } else {
-        //     setErrorMessage({
-        //         name: '',
-        //         height: '',
-        //         weight: '',
-        //         life_span: '',
-        //     })
-        // }
-        
-        
-        // errorMessage.height ? isDisabled[1] = true : isDisabled[1] = false
-
-        //  else {
-        //     setErrorMessage({
-        //         ...errorMessage,
-        //         weight: ''
-        //     })
-        // }
-        // if (errorMessage.weight || !breed.min_weight || !breed.max_weight) {
-        //     isDisabled[2] = true
-        //  } else {
-        //     isDisabled[3] = false
-        //  }
-    // }
-
-    // const validate = (input) => {
-    //     let msg = ""
-    //     if (input.name === 'name') {
-    //         setErrorMessage({
-    //             ...errorMessage,
-    //             name: !/^[a-zA-Z\s]*$/.test(input.value) ? 'Only letters and spaces are allowed' : ''
-    //         })
-    //         errorMessage.name ? isDisabled[0] = true : isDisabled[0] = false
-    //         // setErrorMessage(
-    //         //     !/^[a-zA-Z\s]*$/.test(input.value) ? 'Only letters and spaces are allowed' : ''
-    //         // )
-    //     } else if (input.name === 'min_height' || input.name === 'max_height') {
-    //         if (Number(breed.min_height) > Number(input.value.max_height)) {
-    //             msg = 'Min Height cannot be greater than Max Height'
-    //             setErrorMessage({
-    //                 ...errorMessage,
-    //                 height: msg
-    //             })
-    //         }
-    //         else if (!/^[0-9]*$/.test(breed.min_height) || !/^[0-9]*$/.test(breed.min_height)) msg = 'Only numbers are allowed'
-    //         setErrorMessage({
-    //             ...errorMessage,
-    //             height: msg
-    //         })
-
-    //     }
-    // }
-
+  
     const handleSubmit = (e) => {
         e.preventDefault()
     }
-
+    
+    const isDisabled = !!Object.values(errorMessage).join('') //Checks if there is any error message to enable/disable submit button
+    
     return (
         <div>
             <h2>Create Breed</h2>
@@ -176,27 +105,33 @@ export default function Form() {
 
                 <label>Height</label><br></br>
                 <label>Min: </label>
-                <input name='min_height' type='number' value={breed.min_height} onChange={handleChange}></input>
+                <input name='minHeight' type='number' value={breed.minHeight} onChange={handleChange} ref={minHeightRef}></input>
                 <label>Max: </label>
-                <input name='max_height' type='number' value={breed.max_height} onChange={handleChange}></input>
+                <input name='maxHeight' type='number' value={breed.maxHeight} onChange={handleChange} ref={maxHeightRef}></input>
                 <small>{errorMessage.height}</small>
                 <br></br>
 
                 <label>Weight</label><br></br>
                 <label>Min: </label>
-                <input name='min_weight' type='number' value={breed.min_weight} onChange={handleChange}></input>
+                <input name='minWeight' type='number' value={breed.minWeight} onChange={handleChange} ref={minWeightRef}></input>
                 <label>Max: </label>
-                <input name='max_weight' type='number' value={breed.max_weight} onChange={handleChange}></input>
+                <input name='maxWeight' type='number' value={breed.maxWeight} onChange={handleChange} ref={maxWeightRef}></input>
                 <small>{errorMessage.weight}</small>
                 <br></br>
 
                 <label>Life Span</label><br></br>
                 <label>Min: </label>
-                <input name='min_life_span' type='number' value={breed.min_life_span} onChange={handleChange}></input>
+                <input name='minLifespan' type='number' value={breed.minLifespan} onChange={handleChange} ref={minLifespanRef}></input>
                 <label>Max: </label>
-                <input name='max_life_span' type='number' value={breed.max_life_span} onChange={handleChange}></input>
-                <small>{errorMessage.life_span}</small>
+                <input name='maxLifespan' type='number' value={breed.maxLifespan} onChange={handleChange} ref={maxLifespanRef}></input>
+                <small>{errorMessage.lifespan}</small>
                 <br></br>
+
+                <label>Imgame URL:</label>
+                <input name="image" value={breed.image} onChange={handleChange} ref={imageRef}></input>
+                <small>{errorMessage.image}</small>
+                <br></br><br></br>
+
                 <input type='submit' disabled={isDisabled}></input>
 
             </form>
